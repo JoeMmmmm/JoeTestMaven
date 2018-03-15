a6055d08b1331ef54b3c9056921d950ccfd3c853
joe <347593597@qq.com>
2018-03-15 10:44:54 +0800

    joe blibli

---
+++ FakeBiliBili-master/joe_first_library/src/main/java/joe/example/com/joe_first_library/Util.java
@@ -0,0 +1,10 @@
+package joe.example.com.joe_first_library;
+
+/**
+ * Created by Joe on 2018/3/15 0015
+ */
+public class Util {
+    public static String getClassName() {
+        return "jar 返回的数据";
+    }
+}
